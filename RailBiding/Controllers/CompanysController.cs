33e308d90e82d19cd6981dd1ba1db37b1fe4fd87
Haifu Ge <16964544@qq.com>
2018-07-22 11:28:27 +0800

    0

--- RailBiding/Controllers/CompanysController.cs
+++ RailBiding/Controllers/CompanysController.cs
@@ -769,6 +769,7 @@ namespace RailBiding.Controllers
 
         public ActionResult ReApply()
         {
+            ViewBag.cid = Request["cid"].ToString();
             return View();
         }
     }
