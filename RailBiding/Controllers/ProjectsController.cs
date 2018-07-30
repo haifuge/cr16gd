db39933baaa8849ca63b518b66acf5b4baf06b28
Haifu Ge <16964544@qq.com>
2018-07-30 22:08:27 +0800

    首页sql

--- RailBiding/Controllers/ProjectsController.cs
+++ RailBiding/Controllers/ProjectsController.cs
@@ -132,18 +132,55 @@ namespace RailBiding.Controllers
         public ActionResult MakeBidFile(string pid)
         {
             ViewBag.pid = pid;
-            string sql = @"select mb.ProjId, mb.Abstract, mb.FileExplain, d.Name+' '+ui.UserName as pName
-                            from MakeBidingFile mb 
-                            inner join DepartmentUser du on mb.PublisherId=du.UserId and du.MainDeparment=1
-                            inner join Department d on d.ID=du.DepartmentId
-                            inner join UserInfo ui on ui.ID=du.UserId 
-                            where mb.ProjId=" + pid;
-            DataTable dt = DBHelper.GetDataTable(sql);
+            MakeBidFileContext mc = new MakeBidFileContext();
+            DataTable dt = mc.GetMakeBidFileDetail(pid);
             if(dt.Rows.Count>0)
             {
-                ViewBag.Abstract = dt.Rows[0]["Abstract"].ToString();
-                ViewBag.FileExplain = dt.Rows[0]["FileExplain"].ToString();
-                ViewBag.pName = dt.Rows[0]["pName"].ToString();
+                DataRow dr = dt.Rows[0];
+                ViewBag.PName = dr["Name"].ToString();
+                ViewBag.Publisher = dr["Publisher"].ToString();
+                ViewBag.PublishDate = dr["PublishDate"].ToString();
+                ViewBag.Abstract = dr["Abstract"].ToString().Replace("\r", "    ").Replace("\n", "</br>");
+                ViewBag.FileExplain = dr["FileExplain"].ToString().Replace("\r", "    ").Replace("\n", "</br>");
+
+                dt = mc.GetBidingCompany(pid);
+                string joinCompanys = "";
+                string winCompanys = "";
+                foreach (DataRow row in dt.Rows)
+                {
+                    if (row["Biding"].ToString() == "1")
+                    {
+                        string ConstructionContent = row["ConstructionContent"].ToString();
+                        ConstructionContent = ConstructionContent.Substring(0, ConstructionContent.Length > 40 ? 40 : ConstructionContent.Length);
+                        string QualificationLevel = row["QualificationLevel"].ToString();
+                        QualificationLevel = QualificationLevel.Substring(0, QualificationLevel.Length > 16 ? 16 : QualificationLevel.Length);
+                        joinCompanys += string.Format(@"<li><p class='f16'>{0}</p>
+                                <p>投标报价：<span class='colblue'>{1}元</span></p>
+                                <p>二次报价：<span class='colblue'>{2}元</span></p>
+                                <p>资质等级：{3}</p>
+                                <p>注册资金：{4}万元</p>
+                                <p>{5}</p>
+                            </li>", row["Name"].ToString(), row["FirstPrice"].ToString(), row["SecondPrice"].ToString(), QualificationLevel, row["RegisteredCapital"].ToString(), ConstructionContent);
+                    }
+                    if (row["Win"].ToString() == "1")
+                    {
+                        winCompanys += string.Format(@"<li><h3>{0}</h3><p>{1}</p></li>", row["Name"].ToString(), row["Comment"].ToString());
+                    }
+                }
+                ViewBag.JoinCompany = joinCompanys;
+                ViewBag.WinCompany = winCompanys;
+                ViewBag.moretime = "";
+                if (Request["status"].ToString() == "3")
+                {
+                    if (dr["uid"].ToString() == Session["UserId"].ToString())
+                    {
+                        ViewBag.moretime = "<a href='/Projects/MakeBidFile?pid=" + pid + @"&status=3' class='js-cancle-meet' title='再次申请' ><i class='meet-icon icon-cancel icon-daooutbtn'>再次申请</i></a>";
+                    }
+                    else
+                    {
+                        ViewBag.moretime = "";
+                    }
+                }
             }
             return View();
         }
@@ -615,6 +652,7 @@ namespace RailBiding.Controllers
                     ViewBag.moretime = "";
                 }
             }
+
             return View();
         }
         
