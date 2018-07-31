047d1147758168528be60d8759863f9ee02dae5c
Haifu Ge <16964544@qq.com>
2018-07-31 18:02:55 +0800

    发短信代码，邀标静态页

---
+++ RailBiding/Mobile/MobileInviteBidingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RailBiding.Mobile
+{
+    public class MobileInviteBidingController : Controller
+    {
+        // GET: MobileInviteBiding
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
