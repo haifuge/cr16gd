c88a9631f4e990c0f350343dca0fb6c6f9dfb337
Haifu Ge <16964544@qq.com>
2018-08-01 18:12:48 +0800

    修改密码，移动端登陆

--- AliMessage/SendMessage.cs
+++ AliMessage/SendMessage.cs
@@ -55,5 +55,48 @@ namespace AliMessage
                 return e.ToString();
             }
         }
+
+        public string SendVerifyCode(string phoneNumber, string code)
+        {
+            String product = "Dysmsapi";//短信API产品名称（短信产品名固定，无需修改）
+            String domain = "dysmsapi.aliyuncs.com";//短信API产品域名（接口地址固定，无需修改）
+            String accessKeyId = "LTAI5Txp11mK9Elo";//你的accessKeyId，参考本文档步骤2
+            String accessKeySecret = "1tn32PdmIxWm4wy4Xo2feQCks4fN5v";//你的accessKeySecret，参考本文档步骤2
+            IClientProfile profile = DefaultProfile.GetProfile("cn-hangzhou", accessKeyId, accessKeySecret);
+            //IAcsClient client = new DefaultAcsClient(profile);
+            // SingleSendSmsRequest request = new SingleSendSmsRequest();
+            //初始化ascClient,暂时不支持多region（请勿修改）
+            DefaultProfile.AddEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);
+            IAcsClient acsClient = new DefaultAcsClient(profile);
+            SendSmsRequest request = new SendSmsRequest();
+            try
+            {
+                //必填:待发送手机号。支持以逗号分隔的形式进行批量调用，批量上限为1000个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式，发送国际/港澳台消息时，接收号码格式为00+国际区号+号码，如“0085200000000”
+                request.PhoneNumbers = phoneNumber;
+                //必填:短信签名-可在短信控制台中找到
+                request.SignName = "中铁十六局集团轨道公司";
+                //必填:短信模板-可在短信控制台中找到，发送国际/港澳台消息时，请使用国际/港澳台短信模版
+                request.TemplateCode = "SMS_140665141";
+                //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
+                request.TemplateParam = "{\"code\":\"" + code + "\"}";
+                //可选:outId为提供给业务方扩展字段,最终在短信回执消息中将此值带回给调用者
+                request.OutId = "yourOutId";
+                //请求失败这里会抛ClientException异常
+                SendSmsResponse sendSmsResponse = acsClient.GetAcsResponse(request);
+                System.Console.WriteLine(sendSmsResponse.Message);
+                if (sendSmsResponse.Message == "OK")
+                    return "1";
+                else
+                    return sendSmsResponse.Message;
+            }
+            catch (ServerException e)
+            {
+                return e.ToString();
+            }
+            catch (ClientException e)
+            {
+                return e.ToString();
+            }
+        }
     }
 }
--- RailBiding/Controllers/ForgetPasswordController.cs
+++ RailBiding/Controllers/ForgetPasswordController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DAL.Tools;
+using AliMessage;
 
 namespace RailBiding.Controllers
 {
@@ -16,7 +18,73 @@ namespace RailBiding.Controllers
 
         public ActionResult ResetPassword()
         {
+            string vcode = Request["code"].ToString();
+            Session["uAccount"] = Request["account"].ToString();
+            if (vcode != Session["VCode"].ToString())
+                return View("ForgetPassword");
             return View();
+        }
+        public string CheckNext(string vcode)
+        {
+            if (vcode == Session["VCode"].ToString())
+            {
+                return "1";
+            }
+            else
+                return "0";
+        }
+
+        public string CheckSendMessage(string account, string phone)
+        {
+            string sql = "select count(1) from UserInfo where UserAccount='" + account + "' and Telphone='" + phone + "' and Status = 1;";
+            string i = DBHelper.ExecuteScalar(sql);
+            if (i != "1")
+            {
+                return "用户账号和手机号不匹配";
+            }
+            else
+            {
+                SendMessage sm = new SendMessage();
+                string vcode = GenerateCheckCode();
+                Session["VCode"] = vcode;
+                sm.SendVerifyCode(phone, vcode);
+                return "验证码已发送，请注意查收";
+            }
+        }
+        //随机生成验证码
+        private string GenerateCheckCode()
+        {
+            int number;
+            char code;
+            string checkCode = String.Empty;
+            System.Random random = new Random();
+            for (int i = 0; i < 4; i++)
+            {
+                number = random.Next();
+                if (number % 2 == 0)
+                    code = (char)('0' + (char)(number % 10));
+                else
+                    code = (char)('a' + (char)(number % 26));
+
+                if (code.ToString() == "0" || code.ToString().ToLower() == "o")
+                {
+                    code = 'v';
+                }
+
+                if (code.ToString().ToLower() == "z" || code.ToString().ToLower() == "i" || code.ToString().ToLower() == "b")
+                {
+                    code = '8';
+                }
+                checkCode += code.ToString();
+            }
+            return checkCode;
+        }
+
+        public void UpdateUserPassword(string password)
+        {
+            password = EncryptHelper.Encrypt(password, "IamKey12");
+            string sql = "update UserInfo set Password='"+password+ "' where UserAccount='"+Session["uAccount"].ToString()+"'";
+            DBHelper.ExecuteNonQuery(sql);
         }
     }
 }
