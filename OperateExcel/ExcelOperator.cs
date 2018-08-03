6f08969ab8a41dc3127518c0fc51b9419bdce7e5
Haifu Ge <16964544@qq.com>
2018-08-03 16:45:21 +0800

    非管理员定标文件审批通过

--- OperateExcel/ExcelOperator.cs
+++ OperateExcel/ExcelOperator.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data.OleDb;
-using Microsoft.Office.Core;
-using Microsoft.Office.Interop.Excel;
+﻿using Microsoft.Office.Interop.Excel;
 using System.Reflection;
 
 namespace OperateExcel
