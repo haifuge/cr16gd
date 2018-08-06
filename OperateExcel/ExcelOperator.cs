c80a0865e998330eb238c0dc5d9298df4f15d8c9
Haifu Ge <16964544@qq.com>
2018-08-06 09:59:34 +0800

    导出excel

--- OperateExcel/ExcelOperator.cs
+++ OperateExcel/ExcelOperator.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.Office.Interop.Excel;
 using System.Reflection;
+using System.Text;
+using System;
+using System.IO;
 
 namespace OperateExcel
 {
     public class ExcelOperator
     {
         //将数据写入已存在Excel
-        public static void writeExcel(System.Data.DataTable data, string filepath)
+        public static string writeExcel(System.Data.DataTable data, string filepath)
         {
             Application xApp = new Application();
 
@@ -18,7 +21,6 @@ namespace OperateExcel
 
             //3.指定要操作的Sheet
             Worksheet xSheet = (Worksheet)xBook.Sheets[1];
-
             //在第一列的左边插入一列  1:第一列
             //xlShiftToRight:向右移动单元格   xlShiftDown:向下移动单元格
             //Range Columns = (Range)xSheet.Columns[1, System.Type.Missing];
@@ -27,19 +29,21 @@ namespace OperateExcel
             //4.向相应对位置写入相应的数据
             //xSheet.Cells[0][0] = result;
             int row = 0;
-            for (int i = 0; i < data.Columns.Count; i++)
-                xSheet.Cells[0][i] = data.Columns[i].ColumnName;
+            int column = 0;
             for (int i = 0; i < data.Rows.Count; i++)
             {
-                row = i + 1;
+                row = i + 2;
                 for(int j=0;j<data.Columns.Count;j++)
                 {
-                    xSheet.Cells[row][j] = data.Rows[i][j].ToString();
+                    column = j + 1;
+                    xSheet.Cells[row][column] = data.Rows[i][j].ToString();
                 }
             }
-
+            if (Directory.Exists("/ExcelTemplate/CompanysData"))
+                Directory.CreateDirectory("/ExcelTemplate/CompanysData");
+            string fName = "/ExcelTemplate/CompanysData/Companys" + DateTime.Now.ToShortDateString().Replace(":","").Replace(" ","")+".xlsx";
             //5.保存保存WorkBook
-            xBook.Save();
+            xBook.SaveAs(fName);
             //6.从内存中关闭Excel对象
             xSheet = null;
             xBook.Close();
@@ -49,6 +53,7 @@ namespace OperateExcel
             //Excel从内存中退出
             xApp.Quit();
             xApp = null;
+            return fName;
         }
     }
 }
