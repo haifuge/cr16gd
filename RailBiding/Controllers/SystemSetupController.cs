d19f8de45e52746823d89119fc577f269e84a58e
bainiao <16964544@qq.com>
2018-10-24 14:27:41 +0800

    管理员修改人员信息修复

--- RailBiding/Controllers/SystemSetupController.cs
+++ RailBiding/Controllers/SystemSetupController.cs
@@ -214,7 +214,7 @@ namespace RailBiding.Controllers
         }
         public string UpdateUserInfo()
         {
-            string uid = Session["UserId"].ToString();
+            string uid = Request["uid"].ToString();
             string uname = Request["uname"].ToString();
             string tel = Request["tel"].ToString();
             string psd;
